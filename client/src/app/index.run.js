8d6ccc88e9c3fe6bc7ff414c754b9d8360671f0c
jimtierneysc <jimtierneysc@gmail.com>
2016-06-01 17:44:54 -0700

    Change comments

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -9,6 +9,20 @@
   function runBlock($log) {
 
     $log.debug('runBlock end');
+
+      // // Monitor uirouter state change
+      // $rootScope
+      //   .$on('$stateChangeStart',
+      //     function(event, toState, toParams, fromState, fromParams){
+      //       $log.info('stateChangeStart');
+      //     });
+      //
+      // $rootScope
+      //   .$on('$stateChangeSuccess',
+      //     function(event, toState, toParams, fromState, fromParams){
+      //       $log.info('stateChangeEnd');
+      //     });
+
   }
 
 })();
