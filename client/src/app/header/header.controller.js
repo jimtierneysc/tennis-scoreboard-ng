8d94cb0bfbcc8b6aebae2b5d19bc664dd728d955
jimtierneysc <jimtierneysc@gmail.com>
2016-06-24 21:47:26 -0700

    Remove login form from header

--- client/src/app/header/header.controller.js
+++ client/src/app/header/header.controller.js
@@ -10,14 +10,24 @@
 
   angular
     .module('frontend')
-    .controller('HeaderController', MainController);
+    .controller('HeaderController', Controller);
 
   /** @ngInject */
-  function MainController() {
+  function Controller(authHelper, $scope, $state) {
     var vm = this;
     vm.isCollapsed = true;
-    // TODO: Support login/logout
-    vm.loggedIn = false;
-    vm.userName = "";
+    vm.loggingIn = loggingIn;
+
+    activate();
+
+    function activate() {
+      authHelper.activate(vm, $scope);
+    }
+
+    function loggingIn() {
+      return  (['login', 'register'].indexOf($state.current.name) >= 0)
+    }
+
   }
 })();
+
