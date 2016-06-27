5919184c80237bfc6a001ec68d18ae13eccad693
jimtierneysc <jimtierneysc@gmail.com>
2016-06-26 20:21:19 -0700

    ng-token-auth not used

--- client/src/app/index.module.js
+++ client/src/app/index.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('frontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria',
-      'ngResource', 'ui.router', 'ui.bootstrap', 'ui.bootstrap.modal', 'toastr', 'ui.select', 'ng-token-auth']);
+    .module('frontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngStorage',
+      'ngResource', 'ui.router', 'ui.bootstrap', 'ui.bootstrap.modal', 'toastr', 'ui.select']);
 
 })();
