8a13d0cd3df2d00bbfee899aa02f911602beb8a1
jimtierneysc <jimtierneysc@gmail.com>
2016-07-17 11:43:33 -0700

    Add html reporter   > karma start --reporters html

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -70,7 +70,8 @@ module.exports = function(config) {
       'karma-phantomjs-shim',
       'karma-coverage',
       'karma-jasmine',
-      'karma-ng-html2js-preprocessor'
+      'karma-ng-html2js-preprocessor',
+      'karma-html-reporter'
     ],
 
     coverageReporter: {
@@ -78,11 +79,29 @@ module.exports = function(config) {
       dir : 'coverage/'
     },
 
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
 
     proxies: {
       '/assets/': path.join('/base/', conf.paths.src, '/assets/')
+    },
+
+
+    // the default configuration 
+    htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports  
+      templatePath: null, // set if you moved jasmine_template.html 
+      focusOnFailures: true, // reports show failures on start 
+      namedFiles: false, // name files instead of creating sub-directories 
+      pageTitle: null, // page title for reports; browser info by default 
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs 
+      reportName: 'report-summary-filename', // report summary filename; browser info by default 
+
+
+      // experimental 
+      preserveDescribeNesting: false, // folded suites stay folded  
+      foldAll: false, // reports start folded (only with preserveDescribeNesting) 
     }
+
   };
 
   // This is the default preprocessors configuration for a usage with Karma cli
