03afe6f221e4c8ce450957ea2630112b24e93a57
jimtierneysc <jimtierneysc@gmail.com>
2016-08-18 22:20:11 -0700

    Service to short a name "John Smith"  -> "John S."

---
+++ client/src/app/components/shortenName/shortenName.service.js
@@ -0,0 +1,60 @@
+/**
+ * @ngdoc factory
+ * @name shortenName
+ * @description
+ * Shortens a full firstname and full lastname to full first name and last initial.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('frontendComponents')
+    .factory('shortenName', factory);
+
+  /** @ngInject */
+  function factory() {
+
+    return shortenName;
+
+    function shortenName(name) {
+      var result = name;
+
+      if (result) {
+        // Remove extraneous white space
+        result = result.trim().replace(/ +/g, " ");
+        var names = result.split(" ");
+        if (names.length == 2) {
+          var firstName = names[0];
+          var lastName = names[1];
+          if (!isInitialed(firstName) && !isInitialed(lastName) && !isShortName(lastName) &&
+            isProperNoun(lastName)) {
+            result = firstName + ' ' + lastName.substr(0, 1) + '.';
+          }
+        }
+      }
+      return result;
+    }
+
+    function isShortName(name) {
+      return name.length <= 2;
+    }
+
+    function isInitialed(name) {
+      return name && name.charAt(name.length-1) == '.';
+    }
+
+    function isProperNoun(name) {
+      return name.length >= 2 &&
+        isUpper(name.charAt(0)) &&
+          isLower(name.charAt(1));
+
+      function isUpper(c) {
+        return c.toUpperCase() == c;
+      }
+
+      function isLower(c) {
+        return c.toLowerCase() == c;
+      }
+    }
+  }
+})();
---
+++ client/src/app/components/shortenName/shortenName.service.spec.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict';
+
+  describe('shortenName service', function () {
+    var service;
+
+    beforeEach(module('frontendComponents'));
+    beforeEach(function () {
+      inject(function (_shortenName_) {
+        service = _shortenName_;
+      })
+    });
+
+    it('should be a function', function () {
+      expect(service).toEqual(jasmine.any(Function));
+    });
+
+    describe('when canonical name', function () {
+
+      it('should initialize last name', function () {
+        var value = 'John Smith';
+        var expected = 'John S.';
+        expect(service(value)).toEqual(expected);
+      });
+
+      it('should ignore spaces', function () {
+        var value = ' John   Smith ';
+        var expected = 'John S.';
+        expect(service(value)).toEqual(expected);
+      });
+
+    });
+
+    describe('when initialized names', function () {
+      it('should not shorten when first name is initialized', function () {
+        var value = 'J. Smith';
+        expect(service(value)).toEqual(value);
+      });
+
+      it('should not shorten when last name is initialized', function () {
+        var value = 'John S.';
+        expect(service(value)).toEqual(value);
+      });
+    });
+
+    describe('when last name is not proper nown', function() {
+      it('should not shorten', function() {
+        var value = 'John SMITH';
+        expect(service(value)).toEqual(value);
+      });
+    });
+
+    describe('when last name is short', function() {
+      it('should not shorten', function() {
+        var value = 'John Yo';
+        expect(service(value)).toEqual(value);
+      });
+    });
+
+    describe('when not two names', function () {
+      it('should not shorten when one name', function () {
+        var value = 'John';
+        expect(service(value)).toEqual(value);
+      });
+
+      it('should not shorten when more than two names', function () {
+        var value = 'John Paul Smith';
+        expect(service(value)).toEqual(value);
+      });
+    });
+  });
+
+})();
+
