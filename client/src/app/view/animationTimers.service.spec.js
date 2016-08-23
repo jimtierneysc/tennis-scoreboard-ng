10049b24548c824843e6e06caef9957c5febb866
jimtierneysc <jimtierneysc@gmail.com>
2016-08-22 22:09:33 -0700

    Code coverage

--- client/src/app/view/animationTimers.service.spec.js
+++ client/src/app/view/animationTimers.service.spec.js
@@ -4,28 +4,59 @@
   describe('animationTimers service', function () {
 
     var service;
+    var $timeout;
 
     beforeEach(module('frontendView'));
 
     beforeEach(function () {
 
-      inject(function (_animationTimers_) {
+      inject(function (_animationTimers_, _$rootScope_, _$timeout_) {
         service = _animationTimers_;
+        $timeout = _$timeout_;
       });
     });
 
     describe('members', function () {
 
-      it('should have .deleayIn', function () {
-        expect(server.delayIn).toBe(jasmine.any(Function));
+      it('should have .delayIn()', function () {
+        expect(service.delayIn).toEqual(jasmine.any(Function));
       });
 
-      it('should have .deleayOut', function () {
-        expect(server.delayOut).toBe(jasmine.any(Function));
+      it('should have .delayOut()', function () {
+        expect(service.delayOut).toEqual(jasmine.any(Function));
       });
-      
-      it('should have .digest', function () {
-        expect(server.digest).toBe(jasmine.any(Function));
+
+      it('should have .digest()', function () {
+        expect(service.digest).toEqual(jasmine.any(Function));
+      });
+    });
+
+    describe('promises', function () {
+      var executed;
+      beforeEach(function() {
+        executed = false;
+      });
+
+      function execute() {
+        executed = true;
+      }
+
+      it('should execute .delayIn()', function () {
+        service.delayIn().then(execute);
+        $timeout.flush();
+        expect(executed).toBeTruthy();
+      });
+
+      it('should execute .delayOut()', function () {
+        service.delayOut().then(execute);
+        $timeout.flush();
+        expect(executed).toBeTruthy();
+      });
+
+      it('should execute .digest()', function () {
+        service.digest().then(execute);
+        $timeout.flush();
+        expect(executed).toBeTruthy();
       });
     });
   })
