36f90e86ff2f5bfd5401899a48fba568259ae068
jimtierneysc <jimtierneysc@gmail.com>
2016-08-27 08:44:13 -0700

    Add animateChange service

---
+++ client/src/app/animation/animateChange.service.js
@@ -0,0 +1,147 @@
+/**
+ * @ngdoc factory
+ * @name animateChange
+ * @description
+ * Allow elements in a view to animate by creating
+ * delays between changes to the view.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('frontendAnimation')
+    .factory('animateChange', factory);
+
+  /** @ngInject */
+  function factory($timeout, animationTimers, $q) {
+    return {
+      promiseHideThenShow: promiseHideThenShow,
+      hideThenShow: hideThenShow,
+      toggleShow: toggleShow
+    };
+
+    /**
+     * @ngdoc function
+     * @name promiseHideThenShow
+     * @description
+     * Given a promise and some callbacks,
+     * animate the elements before and after making a change.
+     * @param {Object} promise
+     * A promise to complete before make a change
+     * @param {Function} change
+     * Function to make the change
+     * @param {Object} hideAndShow
+     * Contains Function members: hideChanging, hideChanged, showChanged and reset.
+     * @param {Function} reject
+     * Function called when promise is rejected
+     * @param {Function} final
+     * Function called when promise is finalized
+     */
+    function promiseHideThenShow(promise, change, hideAndShow, reject, final) {
+
+      // Delay to let changing data hide
+      var timerPromise = animationTimers.delayOut();
+
+      var all = $q.all({timer: timerPromise, promise: promise});
+
+      // Set flags so that changing values will be hidden
+      hideAndShow.hideChanging();
+
+      all.then(
+        function (hash) {
+          var response = hash.promise;
+
+          change(response);
+          // Note: There must not be a digest between change() and hideChanged().
+          // Otherwise, the changed elements will be rendered without animation.
+
+          // Set flags so that new elements will be hidden, initially.
+          // Flags also enable ng-class.
+          hideAndShow.hideChanged();
+
+          // Let ng-class be processed
+          animationTimers.digest().then(function () {
+
+            // Show new elements
+            hideAndShow.showChanged();
+
+            // Reset flags after new elements have shown
+            animationTimers.delayIn().then(hideAndShow.reset);
+          });
+
+        },
+        function (response) {
+          if (reject)
+            reject(response);
+          hideAndShow.reset();
+        }).finally(
+        function () {
+          if (final) {
+            final();
+          }
+        });
+    }
+
+    /**
+     * @ngdoc function
+     * @name hideThenShow
+     * @description
+     * Given a change function and some other callbacks,
+     * animate the elements before and after making a change.
+     * @param {Function} change
+     * function to make the change
+     * @param {Object} hideAndShow
+     * Contains Function members: hideChanging, hideChanged, showChanged and reset.
+     */
+    function hideThenShow(change, hideAndShow) {
+
+      hideAndShow.hideChanging();
+
+      animationTimers.delayOut().then(function () {
+
+        change();
+
+        hideAndShow.hideChanged();
+
+        animationTimers.digest().then(function () {
+          hideAndShow.showChanged();
+
+          animationTimers.delayIn().then(
+            hideAndShow.reset);
+        });
+      });
+    }
+
+    /**
+     * @ngdoc function
+     * @name toggleShow
+     * @description
+     * Given a toggle function and two other callbacks, animate the
+     * elements that are to be shown or hidden by toggling a setting.
+     * @param {Function} toggle
+     * Function to toggle a setting
+     * @param {Function} enableNgClass
+     * @param {Function} reset
+     */
+    function toggleShow(toggle, enableNgClass, reset) {
+      // Set flags to enable  ng-class
+      enableNgClass();
+
+      // Eval ng-class
+      animationTimers.digest().then(function () {
+
+        // Change setting
+        var show = toggle();
+
+        // Wait for new elements to finish showing or hiding then
+        // reset flags
+        var delay = show ? animationTimers.delayIn : animationTimers.delayOut;
+        delay().then(reset);
+      });
+    }
+  }
+})();
+
+
+
+
---
+++ client/src/app/animation/animateChange.service.spec.js
@@ -0,0 +1,198 @@
+(function () {
+  'use strict';
+
+  describe('animateChange service', function () {
+
+    var service;
+    var $timeout;
+    var animationTimers;
+    var $rootScope;
+    var $q;
+
+    beforeEach(module('frontendAnimation'));
+
+    beforeEach(function () {
+      module(function ($provide) {
+        // Disable animation delays
+        $provide.factory('animationTimers', function () {
+          return {
+            delayIn: resolvedPromise,
+            delayOut: resolvedPromise,
+            digest: resolvedPromise
+          }
+        });
+      });
+
+      function resolvedPromise() {
+        return $q.resolve();
+      }
+    });
+
+    beforeEach(function () {
+
+      inject(function (_$q_, _animateChange_, _$timeout_, _animationTimers_, _$rootScope_) {
+        service = _animateChange_;
+        $timeout = _$timeout_;
+        animationTimers = _animationTimers_;
+        $rootScope = _$rootScope_;
+        $q = _$q_;
+        spyOn(animationTimers, 'delayIn').and.callThrough();
+        spyOn(animationTimers, 'delayOut').and.callThrough();
+        spyOn(animationTimers, 'digest').and.callThrough();
+      });
+    });
+
+    describe('members', function () {
+
+      it('should have .promiseHideThenShow()', function () {
+        expect(service.promiseHideThenShow).toEqual(jasmine.any(Function));
+      });
+
+      it('should have .promiseHideThenShow()', function () {
+        expect(service.promiseHideThenShow).toEqual(jasmine.any(Function));
+      });
+
+      it('should have .toggleShow()', function () {
+        expect(service.toggleShow).toEqual(jasmine.any(Function));
+      });
+    });
+
+    describe('HideThenShow', function () {
+      var hideAndShow;
+      beforeEach(function () {
+        hideAndShow = {
+          hideChanging: jasmine.createSpy('a'),
+          hideChanged: jasmine.createSpy('b'),
+          showChanged: jasmine.createSpy('c'),
+          reset: jasmine.createSpy('d')
+        };
+      });
+
+      function checkCalled(resolve) {
+        if (angular.isUndefined(resolve))
+          resolve = true;
+
+        it('should have called hideChanging()', function () {
+          expect(hideAndShow.hideChanging).toHaveBeenCalled();
+        });
+
+        it('should have called reset()', function () {
+          expect(hideAndShow.reset).toHaveBeenCalled();
+        });
+
+        var been = resolve ? ' been ' : ' not been ';
+        function expectFn(fn) {
+          var result = expect(fn);
+          if (!resolve)
+            result = result.not;
+          return result;
+        }
+
+        it('should have' + been + 'called hideChanged()', function () {
+          expectFn(hideAndShow.hideChanged).toHaveBeenCalled();
+        });
+
+        it('should have called showChanged()', function () {
+          expectFn(hideAndShow.showChanged).toHaveBeenCalled();
+        });
+      }
+
+      describe('.hideThenShow()', function () {
+        var change;
+        beforeEach(function () {
+          change = jasmine.createSpy('change');
+          service.hideThenShow(change, hideAndShow);
+          $rootScope.$digest();
+        });
+
+        describe('check called', function() {
+          checkCalled();
+        });
+
+        it('should have called change()', function () {
+          expect(change).toHaveBeenCalled();
+        });
+      });
+
+      describe('.promiseHideThenShow()', function () {
+        var change;
+        var final;
+        var reject;
+        var response = {};
+        beforeEach(function () {
+          change = jasmine.createSpy('2');
+          final = jasmine.createSpy('3');
+          reject = jasmine.createSpy('3');
+        });
+
+        describe('resolve', function() {
+          beforeEach(function () {
+            service.promiseHideThenShow($q.resolve(response), change, hideAndShow,
+              reject, final);
+            $rootScope.$digest();
+          });
+
+          describe('check called', function () {
+            checkCalled();
+          });
+
+          it('should have called change()', function () {
+            expect(change).toHaveBeenCalledWith(response);
+          });
+
+          it('should have called final()', function () {
+            expect(final).toHaveBeenCalled();
+          });
+        });
+
+        describe('reject', function() {
+          var response = {};
+          beforeEach(function () {
+            service.promiseHideThenShow($q.reject(response), change, hideAndShow,
+              reject, final);
+            $rootScope.$digest();
+          });
+
+          describe('check called', function () {
+            checkCalled(false);
+          });
+
+          it('should have called reject()', function () {
+            expect(reject).toHaveBeenCalledWith(response);
+          });
+
+          it('should have called final()', function () {
+            expect(final).toHaveBeenCalled();
+          });
+        });
+      });
+    });
+
+    describe('.toggleShow()', function() {
+      var toggle;
+      var enableNgClass;
+      var reset;
+      beforeEach(function () {
+        toggle = jasmine.createSpy('1');
+        enableNgClass = jasmine.createSpy('2');
+        reset = jasmine.createSpy('3');
+        service.toggleShow(toggle, enableNgClass, reset);
+        $rootScope.$digest();
+      });
+
+      it('should have called toggle()', function () {
+        expect(toggle).toHaveBeenCalled();
+      });
+
+      it('should have called enableNgClass()', function () {
+        expect(enableNgClass).toHaveBeenCalled();
+      });
+
+      it('should have called reset()', function () {
+        expect(reset).toHaveBeenCalled();
+      });
+
+    })
+  })
+})();
+
