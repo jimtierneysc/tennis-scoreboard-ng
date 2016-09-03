69802fb0b8ac87480d21cbb9229dc69407c5ecfd
jimtierneysc <jimtierneysc@gmail.com>
2016-09-02 17:03:16 -0700

    Remove unused files

--- client/gulp/dgeni.js
+++
@@ -1,8 +0,0 @@
-// var dgeni = require('gulp-dgeni');
-// var ngdoc = require('dgeni-packages/ngdoc');
-//
-// gulp.task('docs', function () {
-//   return gulp.src(['docs/**/*.ngdoc'])
-//     .pipe(dgeni({packages: [ngdoc]}))
-//     .pipe(gulp.dest('build/docs'));
-// });
