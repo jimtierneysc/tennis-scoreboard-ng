360c85b65c2feb1d7c737ae71be4812ca4dc103f
jimtierneysc <jimtierneysc@gmail.com>
2016-09-04 12:55:20 -0700

    Add readonly attribute to input element of ui-select   Prevents keyboard from showing on Android when selecting matches, players or teams using ui-select

---
+++ client/src/app/components/uiSelectReadOnly/uiSelectReadOnly.directive.js
@@ -0,0 +1,37 @@
+/**
+ * @ngdoc directive
+ * @name app.components.directive:feUiSelectReadOnly
+ * @restrict E
+ * @description
+ * Set the readonly attribute on the input element of a ui-select directive.
+ * This is done to prevent the keyboard from showing on Android, when
+ * the ui-select is focused.  This directive should only be used
+ * when ui-select search-enabled="false", because the user
+ * will not be able to type into a readonly ui-select input.
+ *
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('app.components')
+    .directive('feUiSelectReadOnly', directive);
+
+
+  /** @ngInject */
+  function directive() {
+    var directive = {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var uiSelectInput = element[0].querySelector('input');
+        if (uiSelectInput) {
+          // Add readonly attribute with no value
+          uiSelectInput.setAttribute('readonly', '')
+        }
+      }
+    };
+
+    return directive;
+  }
+})();
